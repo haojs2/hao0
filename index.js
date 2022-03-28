552c856a876cb4c6ea5a2897455b79310d4bd045
haobtjs <haobtjs@gmail.com>
2022-03-28 19:43:03 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express=require('express');
 const app=express();
 const port=process.env.PORT||3000;
 app.get('/',(req,res)=>{
-    return res.send('hello world!123');
+    return res.send('hello world!');
 })
 app.listen(port,()=>{console.log(`localhost: ${port}`)});
