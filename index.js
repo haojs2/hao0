627947021a0f366400323b34574b26ffef178f96
haobtjs <haobtjs@gmail.com>
2022-03-30 16:47:56 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express=require('express');
 const app=express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const port=process.env.PORT||3000;
+
 app.get('/',(req,res)=>{
-    return res.send('hello world!');
+    return res.sendFile(__dirname+'\\index.html');
 })
-app.listen(port,()=>{console.log(`localhost: ${port}`)});
+io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+      io.emit('chat message', msg);
+    });
+  });
+  
+http.listen(port,()=>{console.log(`localhost: ${port}`)});
